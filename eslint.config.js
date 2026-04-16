22484dc96b35616435df5c9fbd249a5000f29b58
Asad Ali <42726454+dmp990@users.noreply.github.com>
2026-04-16 09:25:55 +0100

    chore: add linting and formatting scripts with ESLint and Prettier dependencies

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["test/**/*.js", "eslint.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
